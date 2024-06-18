a361ca6f3b18ed12c3810cbc646b4322cef5ce39
cosimogiove <cosimo18497@gmail.com>
2024-06-18 13:52:13 +0200

    prova

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,18 @@ import * as bootstrap from 'bootstrap'
 import.meta.glob([
     '../img/**'
 ])
+const mix = require('laravel-mix');
+
+mix
+ .sass('resources/sass/app.scss', 'public/css')
+ .js('resources/js/app.js', 'public/js')
+ .version()
+ .options({
+    https: true,
+ });
+ module.exports = {
+    //...
+    output: {
+      publicPath: 'https://' + process.env.APP_URL + '/',
+    },
+  };
