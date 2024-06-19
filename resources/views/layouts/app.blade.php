58271e2a0f88a0d6d202d1b1e4d297dba4f7e8f8
cosimogiove <cosimo18497@gmail.com>
2024-06-19 23:22:52 +0200

    feat:prova tolto body

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
     <!-- script per far funzionare css <!-->
     <link rel="stylesheet" href="{{ asset('build/assets/app-fFAgS7OC.css', 'https') }}">
     <script src="//comanda1-production.up.railway.app/build/assets/app-zP5u6OFH.js"></script>
-    {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" /> --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
